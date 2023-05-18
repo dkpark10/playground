cb83c6bc572746120aa5d2fd12d4c7bb132e0687
dkpark10 <dkpark10@naver.com>
2023-05-18 10:07:16 +0900

    feat: 이미지 페이지 추가

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -1,5 +1,43 @@
 import dynamic from "next/dynamic";
+import { Suspense, lazy } from "react";
+// import Tiny from "@/components/tiny";
 
-export default dynamic(() => Promise.resolve(() => <div>Csr Page</div>), {
-  ssr: false,
-});
+// eslint-disable-next-line no-promise-executor-return
+const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
+
+// const Tiny = lazy(async () => {
+//   await sleep(1000);
+//   return import("@/components/tiny");
+// });
+
+function Loading() {
+  console.log("렌더는 되심???");
+  return (
+    <div style={{ border: "1px solid red" }}>
+      <h1>loading...</h1>
+    </div>
+  );
+}
+
+const DynamicComponent = dynamic(
+  async () => {
+    await sleep(1000);
+    return import("@/components/tiny");
+  },
+  {
+    ssr: false,
+    loading: () => <Loading />,
+  },
+);
+
+export default function CsrSuspense() {
+  return <DynamicComponent />;
+}
+
+// const DynamicHeader = dynamic(() => import('../components/header'), {
+//   loading: () => <p>Loading...</p>,
+// });
+
+// export default function Home() {
+//   return <DynamicHeader />;
+// }

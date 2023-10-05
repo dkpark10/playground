2c6b9bf31ffe4f93d8b811041adb0849eb1d4293
dkpark10 <dkpark10@naver.com>
2023-10-05 12:11:45 +0900

    feat: 캐시 테스트용 api 라우트 하나 더 뚫음

--- app/dynamic/page.tsx
+++ app/dynamic/page.tsx
@@ -3,13 +3,25 @@ import { nextFetchClient } from "@/utils/next-fetch-client";
 export const dynamic = "force-dynamic";
 
 const getTodoData = async () => {
-  const res = await nextFetchClient.get<number>("/api/random", {
-    next: { revalidate: 2 },
-  });
-  return res;
+  // const res = await nextFetchClient.get<number>("/api/random", {
+  //   next: { revalidate: 2 },
+  // });
+  // return res;
+
+  return Promise.all([
+    nextFetchClient.get<number>("/api/random"),
+    nextFetchClient.get<number>("/api/random2", {
+      next: { revalidate: 2 },
+    }),
+  ]);
 };
 
 export default async function NextNext() {
-  const ran = await getTodoData();
-  return <div>{ran}</div>;
+  const [ran1, ran2] = await getTodoData();
+  return (
+    <>
+      <div>{ran1}</div>
+      <div>{ran2}</div>
+    </>
+  );
 }

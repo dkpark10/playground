d37241055f1f794f8b48be72cbb61e5f5a2ecec5
dkpark10 <dkpark10@naver.com>
2023-12-12 01:07:19 +0900

    feat: 전체 영역 처리로직 작성

--- src/utils/editor/text-editor-handler.ts
+++ src/utils/editor/text-editor-handler.ts
@@ -12,18 +12,39 @@ export class TextEditorHandler extends AbstractTextEditorHandler {
 
   public runBold() {
     const clonedContents = this.range.cloneContents();
-    // this.range.deleteContents();
 
-    this.runTextActionFirstRangeNode("bold");
-    this.runTextActionLastRangeNode("bold");
-    // this.runTextActionMiddleRangeNode(clonedContents.childNodes, "bold");
+    const { startContainer, endContainer, startOffset, endOffset } = this.range;
+    this.setRangeAll();
+
+    const firstNode =
+      clonedContents.childNodes.length > 1
+        ? this.getActionFirstNodeAllLine(endContainer, endOffset, "bold")
+        : this.getActionFirstNode(endContainer, startOffset, endOffset, "bold");
+
+    const middleNode = this.getActionMiddleNode(clonedContents.childNodes, "bold");
+    const lastNode = this.getActionLastNode(startContainer, startOffset, "bold");
+
+    this.range.deleteContents();
+
+    this.range.insertNode(firstNode);
+    middleNode.forEach((node) => this.range.insertNode(node));
+
+    if (clonedContents.childNodes.length > 1) {
+      this.range.insertNode(lastNode);
+    }
+
     this.selection.removeAllRanges();
   }
 
-  public runTextActionFirstRangeNode(action: Editor.EditorAction) {
-    const { startContainer, startOffset } = this.range;
-    const beforeText = startContainer.parentElement?.textContent?.slice(0, startOffset);
-    const afterText = startContainer.parentElement?.textContent?.slice(startOffset);
+  /** @desc 영역을 start container, end container 를 포함하여 새로 설정하는 함수 */
+  public setRangeAll() {
+    this.range.setStart(this.range.startContainer, 0);
+    this.range.setEndAfter(this.range.endContainer);
+  }
+
+  public getActionFirstNodeAllLine(node: Node, startOffset: number, action: Editor.EditorAction) {
+    const beforeText = node.textContent?.slice(0, startOffset);
+    const afterText = node.textContent?.slice(startOffset);
 
     const beforeElement = document.createTextNode(beforeText ?? "");
     const afterElement = this.createTextActionElement(action);
@@ -33,13 +54,31 @@ export class TextEditorHandler extends AbstractTextEditorHandler {
     newElement.appendChild(beforeElement);
     newElement.appendChild(afterElement);
 
-    startContainer.parentElement?.replaceWith(newElement);
+    return newElement;
   }
 
-  public runTextActionLastRangeNode(action: Editor.EditorAction) {
-    const { endContainer, endOffset } = this.range;
-    const beforeText = endContainer.parentElement?.textContent?.slice(0, endOffset);
-    const afterText = endContainer.parentElement?.textContent?.slice(endOffset);
+  public getActionFirstNode(node: Node, startOffset: number, endOffset: number, action: Editor.EditorAction) {
+    const beforeText = node.textContent?.slice(0, startOffset);
+    const middleText = node.textContent?.slice(startOffset, endOffset);
+    const afterText = node.textContent?.slice(endOffset);
+
+    const beforeElement = document.createTextNode(beforeText ?? "");
+    const middleElement = this.createTextActionElement(action);
+    middleElement.textContent = middleText ?? "";
+
+    const afterElement = document.createTextNode(afterText ?? "");
+
+    const newElement = document.createElement("div");
+    newElement.appendChild(beforeElement);
+    newElement.appendChild(middleElement);
+    newElement.appendChild(afterElement);
+
+    return newElement;
+  }
+
+  public getActionLastNode(node: Node, endOffset: number, action: Editor.EditorAction) {
+    const beforeText = node.textContent?.slice(0, endOffset);
+    const afterText = node.textContent?.slice(endOffset);
 
     const beforeElement = this.createTextActionElement(action);
     beforeElement.textContent = beforeText ?? "";
@@ -50,24 +89,26 @@ export class TextEditorHandler extends AbstractTextEditorHandler {
     newElement.appendChild(beforeElement);
     newElement.appendChild(afterElement);
 
-    endContainer.parentElement?.replaceWith(newElement);
+    return newElement;
   }
 
-  public runTextActionMiddleRangeNode(middleRangeNode: NodeListOf<ChildNode>, action: Editor.EditorAction) {
+  public getActionMiddleNode(middleRangeNode: NodeListOf<ChildNode>, action: Editor.EditorAction) {
+    const result = [];
     const { length } = middleRangeNode;
-    for (let i = length - 1; i >= 1; i -= 1) {
+    for (let i = length - 2; i >= 1; i -= 1) {
       const childNode = middleRangeNode[i];
-      this.runTextActionRangeNode(childNode, action);
+      result.push(this.runTextActionRangeNode(childNode, action));
     }
+
+    return result;
   }
 
-  public runTextActionRangeNode(childNode: ChildNode | null, action: Editor.EditorAction) {
-    if (!childNode || !childNode.textContent) return;
-
+  public runTextActionRangeNode(childNode: ChildNode, action: Editor.EditorAction) {
     const wrapperElement = this.createTextActionElement(action);
     const newElement = childNode.cloneNode();
     newElement.textContent = childNode.textContent;
     wrapperElement.appendChild(newElement);
-    this.range.insertNode(wrapperElement);
+
+    return wrapperElement;
   }
 }

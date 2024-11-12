1dbd3a1e0b272112b3a3a262659483830b6a9b4a
dkpark10 <dkpark10@naver.com>
2024-11-12 20:27:44 +0900

    style: radius 추가

--- packages/next-img-lib/src/index.tsx
+++ packages/next-img-lib/src/index.tsx
@@ -3,16 +3,25 @@ import { css } from '@emotion/react';
 
 interface NextImageProps extends ImageProps {
   backgroundColor: string;
+  borderRadius: 'md' | 'lg' | 'xl';
 }
 
+const radiusValue: Record<NextImageProps['borderRadius'], string> = {
+  md: '0.375rem',
+  lg: '0.5rem',
+  xl: '0.75rem',
+};
+
 export default function NextImage(props: NextImageProps) {
-  const { backgroundColor, alt, ...rest } = props;
+  const { backgroundColor, alt, borderRadius, ...rest } = props;
+
   return (
     <Image
       alt={alt}
       {...rest}
       css={css`
         background-color: ${backgroundColor};
+        border-radius: ${radiusValue[borderRadius]};
       `}
     />
   );

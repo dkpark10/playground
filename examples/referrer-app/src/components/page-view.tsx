1cb97f7ecc01db25363bd339195b7f6c4d309fab
dkpark10 <dkpark10@naver.com>
2025-12-01 22:52:28 +0900

    test: 레퍼러 테스트 논 ui 모드에서 통과하도록 수정

--- examples/referrer-app/src/components/page-view.tsx
+++ examples/referrer-app/src/components/page-view.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSafeContext } from '@/hooks/use-safe-context';
 import { RefererContext } from '@/components/referer-provider';
 import { statService } from '@/lib/stat-service';
-import { useQueryReferer } from '@/hooks/use-query-referer';
 
 export default function PageView() {
+  const [referer, setReferer] = useState('');
   const { getReferer } = useSafeContext(RefererContext);
-  const referer = useQueryReferer();
-  referer;
 
   useEffect(() => {
     statService.send({
       referer: getReferer(),
     });
+    setReferer(getReferer());
   }, []);
 
-  return <></>;
+  return <div data-testid='referer'>{referer}</div>;
 }
--- examples/referrer-app/tests/referrer.test.ts
+++ examples/referrer-app/tests/referrer.test.ts
@@ -1,45 +1,77 @@
 import { test, expect, type Page } from '@playwright/test';
 
-const checkReferer = async (page: Page, referer: string) => {
-  await page.route('**/api/stat', (route) => {
-    const request = route.request();
-    const postData = request.postData();
-
-    const payload = JSON.parse(postData!) as { referer: string };
-    expect(payload.referer).toBe(referer);
-
-    route.continue();
-  });
-};
-
 test('레퍼러 테스트', async ({ page }) => {
-  checkReferer(page, '');
   await page.goto('/dynamic/22');
+  expect(await page.getByTestId('referer').innerText()).toBe('');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/22');
-  await page.getByRole('link', { name: /다음 페이지 이동/ }).click();
+  await page.getByRole('link', { name: /다음 페이지 이동\s*\d+/ }).click();
   await expect(page).toHaveURL('http://localhost:3000/dynamic/23');
+  expect(await page.getByTestId('referer').innerText()).toBe('http://localhost:3000/dynamic/22');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/23');
-  await page.getByRole('link', { name: /다음 페이지 이동/ }).click();
+  await page.getByRole('link', { name: /다음 페이지 이동\s*\d+/ }).click();
   await expect(page).toHaveURL('http://localhost:3000/dynamic/24');
+  expect(await page.getByTestId('referer').innerText()).toBe('http://localhost:3000/dynamic/23');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/24');
   await page.getByRole('link', { name: /다음 페이지 이동/ }).click();
   await expect(page).toHaveURL('http://localhost:3000/dynamic/25');
+  expect(await page.getByTestId('referer').innerText()).toBe('http://localhost:3000/dynamic/24');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/25');
   await page.getByRole('button', { name: /뒤로/ }).click();
   await expect(page).toHaveURL('http://localhost:3000/dynamic/24');
+  expect(await page.getByTestId('referer').innerText()).toBe('http://localhost:3000/dynamic/25');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/24');
   await page.getByRole('button', { name: /뒤로/ }).click();
   await expect(page).toHaveURL('http://localhost:3000/dynamic/23');
+  expect(await page.getByTestId('referer').innerText()).toBe('http://localhost:3000/dynamic/24');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/23');
   await page.getByRole('button', { name: /앞으로/ }).click();
   await expect(page).toHaveURL('http://localhost:3000/dynamic/24');
+  expect(await page.getByTestId('referer').innerText()).toBe('http://localhost:3000/dynamic/23');
 
-  checkReferer(page, 'http://localhost:3000/dynamic/23');
   await page.getByRole('button', { name: /클릭 통계 버튼/ }).click();
 });
+
+
+// const checkReferer = async (page: Page, referer: string) => {
+//   await page.route('**/api/stat', (route) => {
+//     const request = route.request();
+//     const postData = request.postData();
+
+//     const payload = JSON.parse(postData!) as { referer: string };
+//     expect(payload.referer).toBe(referer);
+
+//     route.continue();
+//   });
+// };
+
+// test('레퍼러 테스트', async ({ page }) => {
+//   checkReferer(page, '');
+//   await page.goto('/dynamic/22');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/22');
+//   await page.getByRole('link', { name: /다음 페이지 이동/ }).click();
+//   await expect(page).toHaveURL('http://localhost:3000/dynamic/23');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/23');
+//   await page.getByRole('link', { name: /다음 페이지 이동/ }).click();
+//   await expect(page).toHaveURL('http://localhost:3000/dynamic/24');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/24');
+//   await page.getByRole('link', { name: /다음 페이지 이동/ }).click();
+//   await expect(page).toHaveURL('http://localhost:3000/dynamic/25');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/25');
+//   await page.getByRole('button', { name: /뒤로/ }).click();
+//   await expect(page).toHaveURL('http://localhost:3000/dynamic/24');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/24');
+//   await page.getByRole('button', { name: /뒤로/ }).click();
+//   await expect(page).toHaveURL('http://localhost:3000/dynamic/23');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/23');
+//   await page.getByRole('button', { name: /앞으로/ }).click();
+//   await expect(page).toHaveURL('http://localhost:3000/dynamic/24');
+
+//   checkReferer(page, 'http://localhost:3000/dynamic/23');
+//   await page.getByRole('button', { name: /클릭 통계 버튼/ }).click();
+// });
